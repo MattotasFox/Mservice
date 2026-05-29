32e013db09a673359b14d0ac0e551892573f344c
Mattotas <mitchell.jacke877@gmail.com>
2026-05-28 20:51:57 -0400

    Integración de Capacitator

---
+++ android/app/src/main/java/com/Mservice/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Mservice.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Mservice.app',
+  appName: 'Mservice',
+  webDir: 'dist'
+};
+
+export default config;
